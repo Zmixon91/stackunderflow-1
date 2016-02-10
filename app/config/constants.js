c9fe8755dda91ccf591d4874a622de2fd0297811
Timothy L Taddicken <Timothy L Taddicken>
2016-02-10 13:23:54 -0700

    dashboard inputs

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,6 +1,6 @@
 app.constant('CONSTANTS', (function(){ 
-	var root = 'https://stackunderflow.firebaseio.com/';
-	var questions = root + 'questions/'
+	var root = 'https://realstackunderflow.firebaseio.com/';
+	var questions = root + 'questionList/'
 	var tags = root + 'tags/' 
 	
 	return {
